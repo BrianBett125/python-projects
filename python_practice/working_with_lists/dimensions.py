f26d9e54a3d43c59ad9ee6aae741201d20db0bf5
BrianBett125 <brianbett756@gmail.com>
2025-02-21 10:04:51 +0300

    updated

---
+++ python_practice/working_with_lists/dimensions.py
@@ -0,0 +1,9 @@
+# working with tuples, tuples are immutable(unchangeable)
+dimensions = (400, 200)
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (30, 50)
+print("\nnew dimensions are: ")
+for dimension in dimensions:
+    print(dimension)
---
+++ python_practice/working_with_lists/tuple_dimension.py
@@ -0,0 +1,4 @@
+# tuples
+dimension = (90, 77)
+print(dimension[0])
+print(dimension[1])
