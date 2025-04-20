c315eec6d2383b4cafe6f92d33e870ff762c5e9e
BrianBett125 <briankipkoech756@gmail.com>
2025-04-20 14:32:13 +0300

    updated

---
+++ working_with_lists/buffet.py
@@ -0,0 +1,11 @@
+# defining tuples
+foods = ('chapati', 'mokimo', 'maharagwe', 'mandazi', 'chips')
+print("\nthe restaurant offers")
+for food in foods:
+    print(food)
+
+# new tuple
+foods = ('cake', 'salad', 'maharagwe', 'mandazi', 'chips')
+print("\nthe new buffet menu:")
+for food in foods:
+    print(food)
