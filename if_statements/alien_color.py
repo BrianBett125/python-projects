1d7b19451a8c276f7765ce3831695a649999d779
BrianBett125 <brianbett756@gmail.com>
2025-05-08 23:13:14 +0300

    updated

---
+++ if_statements/alien_color.py
@@ -0,0 +1,26 @@
+#  Create a variable called alien_color and assign it a value
+# of 'green', 'yellow', or 'red'
+
+# Write an if statement to test whether the alien’s color is green. If it is, print 
+# a message that the player just earned 5 points.
+alien_color = 'green'
+if alien_color == 'green':
+    print("you just earned 5 points")
+
+# Write one version of this program that passes the if test and another that fails
+if alien_color == 'red':
+    print("you just earned 5 points")
+
+# Choose a color for an alien as you did in Exercise 5-3, 
+# and write an if-else chain.
+
+# if the alien’s color is green, print a statement that the player just earned 5 
+# points for shooting the alien. If the alien’s color isn’t green,
+# print a statement that the player just earned 
+10 points.
+alien_color = 'yellow'
+if alien_color == 'yellow':
+    print("\nthe player just earned 5 points for shootin down the alien")
+else:
+    print("\nthe player just earned 10 point")
+
---
+++ if_statements/toppings1.py
@@ -0,0 +1,9 @@
+# testing multiple conditions using if statement
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("adding mushrooms")
+if 'pepperoni' in requested_toppings:
+    print("adding pepperoni")
+if 'extra cheese' in requested_toppings:
+    print("adding extra cheese")
+print("\nfinished making your pizza")
