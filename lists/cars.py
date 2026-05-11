8703984fcf2152c5d78eae38d61f964fb5a2a9c1
Brian Kipkoech Bett <brianbett756@gmail.com>
2026-05-11 22:28:42 +0300

    Clean up cars.py by removing unused code
    Removed commented-out code and extra print statements.

--- lists/cars.py
+++ lists/cars.py
@@ -5,13 +5,3 @@ print(cars)
 print("\nhere is the list sorted alphabetically")
 cars.sort()
 print(cars)
-
-
-
-# sorting a list permanently in alphabetical order
-cars = ['bentley', 'audi', 'toyota', 'lexus']
-print(cars)
-
-print("\nhere is the list sorted alphabetically")
-cars.sort()
-print(cars)
